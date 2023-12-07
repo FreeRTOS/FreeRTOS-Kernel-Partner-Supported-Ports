7cab8bb51c57003eeb0b5be9d8b80da6ef3b1716
Ching-Hsin Lee <chinglee@amazon.com>
2023-12-07 18:53:51 +0800

    Update for get passive idle task memory

--- TI/CORTEX_A53_64-BIT_TI_AM64_SMP/port.c
+++ TI/CORTEX_A53_64-BIT_TI_AM64_SMP/port.c
@@ -278,45 +278,40 @@ void vApplicationStackOverflowHook( TaskHandle_t xTask,
 }
 
 #if ( configSUPPORT_STATIC_ALLOCATION == 1 ) && ( configKERNEL_PROVIDED_STATIC_MEMORY == 0 )
-    #if ( configNUMBER_OF_CORES == 1 )
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+    /* configSUPPORT_STATIC_ALLOCATION is set to 1, so the application must provide an
+     * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+     * used by the Idle task.
+     */
+        void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
+                                    StackType_t ** ppxIdleTaskStackBuffer,
+                                    uint32_t * pulIdleTaskStackSize )
+    {
         static StaticTask_t xIdleTaskTCB;
         static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-        /* configSUPPORT_STATIC_ALLOCATION is set to 1, so the application must provide an
-         * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
-         * used by the Idle task.
-         */
-        void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
-                                            StackType_t **ppxIdleTaskStackBuffer,
-                                            uint32_t *pulIdleTaskStackSize )
+
+        *ppxIdleTaskTCBBuffer = &( xIdleTaskTCB );
+        *ppxIdleTaskStackBuffer = &( uxIdleTaskStack[ 0 ] );
+        *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+    }
+
+    #if ( configNUMBER_OF_CORES > 1 )
+
+        void vApplicationGetPassiveIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
+                                                   StackType_t ** ppxIdleTaskStackBuffer,
+                                                   uint32_t * pulIdleTaskStackSize,
+                                                   BaseType_t xPassiveIdleTaskIndex )
         {
-            /* Pass out a pointer to the StaticTask_t structure in which the Idle task’s
-             * state will be stored.
-             */
-            *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+            static StaticTask_t xIdleTaskTCBs[ configNUMBER_OF_CORES - 1 ];
+            static StackType_t uxIdleTaskStacks[ configNUMBER_OF_CORES - 1 ][ configMINIMAL_STACK_SIZE ];
 
-            /* Pass out the array that will be used as the Idle task’s stack. */
-            *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-            /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-             * Note that, as the array is necessarily of type StackType_t,
-             * configMINIMAL_STACK_SIZE is specified in words, not bytes.
-             */
+            *ppxIdleTaskTCBBuffer = &( xIdleTaskTCBs[ xPassiveIdleTaskIndex ] );
+            *ppxIdleTaskStackBuffer = &( uxIdleTaskStacks[ xPassiveIdleTaskIndex ][ 0 ] );
             *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
         }
-    #else
-        void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
-                                            StackType_t ** ppxIdleTaskStackBuffer,
-                                            uint32_t * pulIdleTaskStackSize,
-                                            BaseType_t xCoreId )
-        {
-            static StaticTask_t xIdleTaskTCBs[ configNUMBER_OF_CORES ];
-            static StackType_t uxIdleTaskStacks[ configNUMBER_OF_CORES ][ configMINIMAL_STACK_SIZE ];
 
-            *ppxIdleTaskTCBBuffer = &( xIdleTaskTCBs[ xCoreId ] );
-            *ppxIdleTaskStackBuffer = &( uxIdleTaskStacks[ xCoreId ][ 0 ] );
-            *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-        }
-    #endif
+    #endif /* #if ( configNUMBER_OF_CORES > 1 ) */
 
     static StaticTask_t xTimerTaskTCB;
     static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
